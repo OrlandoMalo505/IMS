0c8af5a71504ea8791125046defe57499a242370
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-03-31 17:56:12 +0200

    added business, use case and plugin layer

--- IMS.UseCases/ViewInventoriesByName.cs
+++ IMS.UseCases/ViewInventoriesByName.cs
@@ -1,7 +1,19 @@
-﻿namespace IMS.UseCases
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases
 {
     public class ViewInventoriesByName
     {
+        private readonly IInventoryRepository _inventoryRepository;
 
+        public ViewInventoriesByName(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name)
+        {
+            return await _inventoryRepository.GetInventoriesByName(name);
+        }
     }
 }
