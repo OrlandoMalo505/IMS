6ddde1d7de567a3504d2afe409ca7c7b525550ed
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-04-05 10:45:34 +0200

    small fixes

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -9,10 +9,10 @@ namespace IMS.CoreBusiness
         [Required]
         public string? InventoryName { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to [0]")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
 
-        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater or equal to [0]")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
     }
 }
